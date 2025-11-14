4d5485f849422f3a494b553cd730a316b9bd9cfb
mgarciaaa2006-cyber <mgarciaaa2006@gmail.com>
2025-11-15 01:26:06 +0800

    WIP ni 7

---
+++ LA6 GUI/src/SimpleCalculator/simpleCalculator.java
@@ -0,0 +1,11 @@
+package SimpleCalculator;
+
+import javax.swing.*;
+
+public class simpleCalculator extends JFrame {
+
+    static void main(String[] args) {
+        new simpleCalculator().setVisible(true);
+    }
+}
+
